19e48e5c538bf3b4bb176fc31ddb658ca9c0700d
hyyi1 <hyyi1@i9i8.com>
2021-06-30 19:57:23 +0800

    初始化

---
+++ src/PhoneCheck/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by phone-check.
+ * User: ogenes
+ * Date: 2021/6/30
+ */
+
+namespace Ogenes\PhoneCheck;
+
+
+class Client
+{
+
+    public static function check() {
+        print_r('ok');
+    }
+}
